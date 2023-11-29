9512f4714054ee2c997c98dd591bb74853c96331
EsterAmorim <eamorimsouza@outlook.com>
2023-11-29 04:21:32 -0300

    commit

---
+++ lib/cidades.dart
@@ -0,0 +1,15 @@
+class Paroquia {
+  String nome;
+  String endereco;
+  String foto;
+  double latitude;
+  double longitude;
+
+  Paroquia({
+    required this.nome,
+    required this.endereco,
+    required this.foto,
+    required this.latitude,
+    required this.longitude
+    });
+}
--- lib/cidades_controller.dart
+++ lib/cidades_controller.dart
@@ -1,15 +1,1 @@
-class Paroquia {
-  String nome;
-  String endereco;
-  String foto;
-  double latitude;
-  double longitude;
 
-  Paroquia({
-    required this.nome,
-    required this.endereco,
-    required this.foto,
-    required this.latitude,
-    required this.longitude
-    });
-}
